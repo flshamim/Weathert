7f729586dd71f09bab40428cef97dd8c39ec7b6a
flshamim <fls67585183@gmail.com>
2022-03-14 23:18:16 +0600

    six commit

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const tempt= () =>{
-    const cityName = document.getElementById('weather').value='';   
+const searchTemp = () =>{
+   const city= document.getElementById('weather').value='';
+   const cityName =document.getElementById('city-name')
+   cityName.innerText=city
+   
+   fetch(`https://goweather.herokuapp.com/weather/${city}`)
+        .then(res => res.json())
+        .then(data => displsyView(data))
+}
+
+const displsyView =(data)=>{
+    console.log(data)
+   
+    const hot = document.getElementById('temp')
+    const dscribe =document.getElementById('adrota')
+   
+    dscribe.innerText=`${data.description}`
+    hot.innerText=`${data.temperature}`
+
+ const city= document.getElementById('weather').value='';
 }
